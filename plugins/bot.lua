b5568c6f173706c6eb61cd203624784713059e76
roboxt <robo.xt7aj@gmail.com>
2017-01-08 02:01:25 +0330

    Update bot.lua

--- plugins/bot.lua
+++ plugins/bot.lua
@@ -53,7 +53,7 @@ local function pre_process(msg)
 	
 	-- If sender is sudo then re-enable the channel
 	if is_sudo(msg) then
-	  if msg.text == "#bot on" or msg.text == "!bot on" or msg.text == "/bot on" then
+	  if msg.text == "ربات روشن" or msg.text == "ربات روشن" or msg.text == "ربات روشن" then
 	    enable_channel(receiver, msg.to.id)
 	  end
 	end
@@ -66,14 +66,14 @@ local function pre_process(msg)
 end
 
 local function run(msg, matches)
-	if permissions(msg.from.id, msg.to.id, "bot") then
+	if permissions(msg.from.id, msg.to.id, "ربات") then
 		local receiver = get_receiver(msg)
 		-- Enable a channel
-		if matches[1] == 'on' then
+		if matches[1] == 'روشن' then
 			return enable_channel(receiver, msg.to.id)
 		end
 		-- Disable a channel
-		if matches[1] == 'off' then
+		if matches[1] == 'خاموش' then
 			return disable_channel(receiver, msg.to.id)
 		end
 	else
@@ -83,8 +83,8 @@ end
 
 return {
 	patterns = {
-		"^[!/# .]bot? (on)",
-		"^[!/# .]bot? (off)" }, 
+		"^ربات? (روشن)",
+		"^ربات? (خاموش)" }, 
 	run = run,
 	pre_process = pre_process
 }

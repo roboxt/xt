132745a01659348d9d1030d36aca4d107dc565dc
roboxt <robo.xt7aj@gmail.com>
2017-01-08 03:10:35 +0330

    Update logo2.lua

--- plugins/logo2.lua
+++ plugins/logo2.lua
@@ -2,7 +2,7 @@ do
 local function run(msg, matches)
     local receiver = get_receiver(msg)
     local site = matches[2]
- local url = "http://logo.clearbit.com/"..site.."?size=500"
+ local url = "http://api.monsterbot.ir/"..site.."?size=500"
  local randoms = math.random(1000,900000)
  local randomd = randoms..".jpg"
  local file = download_to_file(url,randomd)

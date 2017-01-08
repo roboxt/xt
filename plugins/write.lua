2d8b50973105f6984684f29e6914bf56dd0d7a6e
roboxt <robo.xt7aj@gmail.com>
2017-01-08 06:43:58 +0330

    Update write.lua

--- plugins/write.lua
+++ plugins/write.lua
@@ -186,7 +186,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."@GoldTeam"
+	return result_text.."plugins BY XT @SHAHIN_XtBot"
 end
 
 return {
@@ -194,8 +194,8 @@ return {
 	usagehtm = '<tr><td align="center">write متن</td><td align="right">با استفاده از این پلاگین میتوانید متون خود را با فونت های متنوع و زیبایی طراحی کنید. حد اکثر کاراکتر های مجاز 20 عدد میباشد و فقط میتوانید از حروف انگلیسی و اعداد استفاده کنید</td></tr>',
 	usage = {"writer [text] : زیبا نویسی",},
 	patterns = {
-		"^([!/#]writer) (.*)",
-		"^([!/#]writer)$",
+		"^([!/#]yaz) (.*)",
+		"^([!/#]yaz)$",
 		},
 	run = run
 }
